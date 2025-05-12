f4bb921bc55c9349e952a8f5010f8d791e23567c
Daniel Shirabe <scodeiswhite1998@gmail.com>
2025-05-12 22:51:26 +0900

    User rpsec test

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,4 +1,10 @@
 FactoryBot.define do
   factory :user do
+    nickname { 'Daniel' }
+    sequence :email do |n|
+      "test#{n}@example.com"
+    end
+    password { '123456' }
+    password_confirmation { '123456' }
   end
 end
