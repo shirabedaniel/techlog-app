0d2c2da74bfc23ad0f51e0d1c6953f83736f2396
Daniel Shirabe <scodeiswhite1998@gmail.com>
2025-05-23 23:37:29 +0900

    Postモデルを作成

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,7 +8,8 @@ describe User do
 
   describe '.first' do
     before do
-      create(:user, nickname: nickname, email: email)
+      @user = create(:user, nickname: nickname, email: email)
+      @post = create(:post, title: 'タイトル', content: '本文', user_id: @user.id)
     end
 
     subject { described_class.first }
@@ -17,6 +18,13 @@ describe User do
       expect(subject.nickname).to eq('Daniel')
       expect(subject.email).to eq('test@example.com')
     end
+
+    it '紐づくPostの情報を取得する' do
+      expect(subject.posts.size).to eq(1)
+      expect(subject.posts.first.title).to eq('タイトル')
+      expect(subject.posts.first.content).to eq('本文')
+      expect(subject.posts.first.user_id).to eq(@user.id)
+    end  
   end
 
   describe 'validation' do
